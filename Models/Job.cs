39a89a4dcf599efdc084970a14a24186562618f3
MehmetGulbahar <121450823+MehmetGulbahar@users.noreply.github.com>
2024-12-08 17:53:43 +0300

    add Job model

---
+++ Models/Job.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE307_Project.Models
+{
+    public class Job : UniqueIdNode
+    {
+        private UniqueIdList taskList;
+
+        public Job(string id) : base(id)
+        {
+            this.taskList = new UniqueIdList();
+        }
+
+        public Job(string id, UniqueIdList taskList) : base(id)
+        {
+            this.taskList = taskList;
+        }
+
+        public UniqueIdList GetTaskList()
+        {
+            return taskList;
+        }
+
+        public void SetTaskList(UniqueIdList taskList)
+        {
+            this.taskList = taskList;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("---------");
+            Console.WriteLine("Job - " + GetId());
+            Console.WriteLine("---------");
+            Console.WriteLine("Tasks in Job: ");
+            taskList.PrintList();
+        }
+    }
+
+}
